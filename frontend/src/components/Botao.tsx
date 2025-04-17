bb700626022a5730a0e9f09be132363f2d9f2fd6
Madu25ss <eduardaserpa25@gmail.com>
2025-04-16 21:19:48 -0400

    validacaoDoCpf

--- frontend/src/components/Botao.tsx
+++ frontend/src/components/Botao.tsx
@@ -5,7 +5,10 @@ type BotaoProps = {
 
 const Botao = (props: BotaoProps) => {
   return (
-    <button type="button" className="rounded bg-blue-400 text-white py-1 px-2 mt-2 cursor-pointer active:transform-[scale(1.1)] hover:bg-blue-600" onClick={props.onClick}>
+    <button 
+    type="button" 
+    className="rounded bg-blue-400 text-white py-1 px-2 mt-2 cursor-pointer transition delay-100 duration-300 ease-in-out hover:bg-blue-600 hover:-translate-y-1" 
+    onClick={props.onClick}>
         Enviar
     </button>
   )
--- frontend/src/pages/ValidadorCpf.tsx
+++ frontend/src/pages/ValidadorCpf.tsx
@@ -7,8 +7,40 @@ const ValidadorCpf = () => {
     const [cpf, setCpf] = useState<string>("");
     
     const validaCpf = () => {
-        console.log(cpf)
-        //insira aqui sua logica de cpf
+        console.log(`cpf original ${cpf}`);
+        
+        // const cpfNumber = Number(cpf);
+        // console.log(typeof cpfNumber);
+
+        function validacao() {
+            //tira os digitos verificadores, faz o cálculo, adiciona no novoCpf e compara (true, false)
+            const cpfParcial = cpf.slice(0,-2);
+            const digito1 = criaDigito(cpfParcial);
+            const digito2 = criaDigito(cpfParcial + digito1);
+
+            const novoCpf = cpfParcial + digito1 + digito2;
+            console.log(`CPF Validado: ${novoCpf}`);
+            return novoCpf === cpf;
+        }
+
+        function criaDigito(cpfParcial: any) {
+            const cpfArray = Array.from(cpfParcial);
+            let regressivo = cpfArray.length + 1;
+
+            let total = cpfArray.reduce((ac: number, val) => {
+                ac += (regressivo * Number(val));
+                regressivo--;
+                return ac;
+            }, 0);
+
+            let digito = 11 - (total % 11);
+            digito > 9? digito = 0 : digito;
+            return String(digito);
+        };
+
+        let resultado = validacao();
+        
+        console.log(`CPF `, resultado == true? `Válido` : `Inválido`);
     }
     
 
