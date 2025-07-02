b31a126fe9a913a4108643464738cbd2d42a1d84
Maria Eduarda Serpa Santos <mserpa@PSG.LOCAL>
2025-07-01 23:44:00 -0400

    fimTabCPF

--- frontend/src/components/Accordion.tsx
+++ frontend/src/components/Accordion.tsx
@@ -3,41 +3,39 @@ import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "./ui/accordion"
-
+} from "./ui/accordion";
+import { ScrollArea, ScrollBar } from "./ui/scroll-area"
 
 type propsAcco = {
-  disabled?: boolean,
-  textInfos: string
-}
-
-
+  disabled?: boolean;
+  textInfos: string[];
+};
 
 export default function AccordionDemo(props: propsAcco) {
   let retornoComponentes;
-  if (props.disabled == false) {
-    retornoComponentes = <Accordion
-      type="single"
-      collapsible
-      className="w-full max-h-35 text-sm text-zinc-500 bg-slate-100 rounded border border-solid border-neutral-400"
-      defaultValue="item-1"
-    >
-      <AccordionItem value="item-1" className="max-h-35 pr-1 ">
-        <AccordionTrigger>Dados</AccordionTrigger>
-        <AccordionContent className="flex flex-col gap-4 text-balance max-h-20">
-          <p>
+
+  if (props.disabled == false ) {
+    retornoComponentes = (
+      <Accordion
+        type="single"
+        collapsible
+        className="text-sm text-zinc-600 bg-slate-100 rounded border border-neutral-400 w-full "
+      >
+        <AccordionItem value="item-1">
+          <AccordionTrigger className="w-full h-8">Dados</AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-2 text-pretty border w-full whitespace-nowrap ">
             
-          </p>
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
+            {props.textInfos.map((line, index) => (
+              <p key={index}>{line}</p>
+            ))}
+            
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    );
   } else {
     retornoComponentes;
   }
 
-  return (
-    <div>
-    {retornoComponentes}
-    </div>
-  )
+  return <div>{retornoComponentes}</div>;
 }
--- frontend/src/components/ValidadorCpf.tsx
+++ frontend/src/components/ValidadorCpf.tsx
@@ -4,9 +4,7 @@ import Input from "./Input";
 import TextoLink from "./Texto";
 import TextoDesc from "./TextoDesc";
 import Validacao from "./Validacao";
-import Accordion from "./Accordion"
-
-
+import Accordion from "./Accordion";
 
 const ValidadorCpf = () => {
   const [cpf, setCpf] = useState<string>("");
@@ -16,13 +14,10 @@ const ValidadorCpf = () => {
     undefined
   );
 
-  const [retornoJson, setRetornoJson] = useState<string>("");
+  const [retornoJson, setRetornoJson] = useState<string[]>([]);
   const [NaoExibir, SetNaoExibir] = useState<boolean | undefined>(undefined);
 
-
-
   const validaCpf = async () => {
-
     let sucesso;
 
     function ValidaInputFunc(cpf: string) {
@@ -37,7 +32,6 @@ const ValidadorCpf = () => {
 
     if (!dataNascimento) {
       function validacao() {
-
         function isSequencia(cpfNumero: string) {
           if (validaInput == true && cpfNumero != undefined) {
             const sequencia = cpfNumero[0].repeat(cpfNumero.length);
@@ -49,7 +43,6 @@ const ValidadorCpf = () => {
 
         const sequenciaCpf = isSequencia(cpf);
         SetNaoExibir(true);
-      
 
         if (sequenciaCpf == true) {
           sucesso = false;
@@ -59,7 +52,7 @@ const ValidadorCpf = () => {
           const digito2 = criaDigito(cpfParcial + digito1);
 
           const novoCpf = cpfParcial + digito1 + digito2;
-        
+
           return novoCpf === cpf;
         }
       }
@@ -91,29 +84,24 @@ const ValidadorCpf = () => {
         cpf: cpf,
         nascimento: dataNascimento,
       }),
-    });  
+    });
 
     const dados = await resposta.json();
-  
+
     if (resposta.status == 200) {
       sucesso = dados.sucess;
 
-      // const dadosJson = 
-      //   <p>
-      //     `Nome: ${dados.data.nome}` <br />
-      //     `Nascimento: ${dados.data.nascimento}` <br />
-      //     `CPF: ${dados.data.cpf}` <br />
-      //     `Situaçao: ${dados.data.situacao}` <br />
-      //   </p>
-      
+      const geraJson = [
+        `Nome: ${dados.data.nome} \n`,
+        `Nascimento: ${dados.data.nascimento} \n`,
+        `CPF: ${dados.data.cpf} \n`,
+        `Situação: ${dados.data.situacao} \n`,
+      ];
 
-
-      // setRetornoJson(dadosJson);
-
-
-
-
+      setRetornoJson(geraJson);
       SetNaoExibir(false);
+    } else {
+      SetNaoExibir(true);
     }
 
     function validaSucesso(varSucesso: boolean) {
@@ -126,21 +114,14 @@ const ValidadorCpf = () => {
     }
 
     setResultado(validaSucesso(sucesso));
-
-    console.log(`resultado setResultado: ${resultado}`);
-    console.log(`resultado dados.sucess: ${dados.sucess}`);
-    console.log(`resultado tipo sucesso: ${typeof sucesso}`);
-    console.log(`resultado sucesso: ${sucesso}`);
-    console.log(`resultado dados: ${dados}`);
-    console.log(`resultado validaInput: ${validaInput}`);
-    console.log(dados.data.nome)
+    console.log(resultado);
   };
 
   return (
     <>
       <TextoDesc name="CPF" />
-      <div className="flex flex-row space-x-10 w-full h-auto mb-2">
-        <div className="flex flex-col w-60 h-auto space-y-6">
+      <div className="flex flex-row space-x-10 w-full mb-4 ">
+        <div className="flex flex-col w-60 space-y-10 h-auto justify-items-start">
           <Input
             name={"CPF"}
             value={cpf}
@@ -163,11 +144,13 @@ const ValidadorCpf = () => {
           />
         </div>
 
-        <div className=" flex flex-col w-60 h-fit max-h-35 mt-5">
-          <Accordion disabled={NaoExibir} textInfos={retornoJson}/>
+        <div className=" flex flex-col w-full max-w-70 mt-4.5">
+          <Accordion
+            disabled={NaoExibir}
+            textInfos={retornoJson}
+          />
         </div>
       </div>
-
 
       <Validacao validacao={resultado} />
 
